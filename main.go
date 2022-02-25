579e0d6c8fc6e044d0904bfd711d2993c4b2f80c
amirmojarad <amirmoajrad13@gmail.com>
2022-02-25 13:17:43 +0330

    added variables from .env

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"imdb-movie/api"
 	"imdb-movie/ent"
 	"log"
@@ -21,7 +22,8 @@ func init() {
 }
 
 func main() {
-	client, err := ent.Open("postgres", "user=postgres dbname=imdb-movie password=100990729 sslmode=disable")
+	username, password := os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_USERNAME")
+	client, err := ent.Open("postgres", fmt.Sprintf("user=%s dbname=imdb-movie password=%s sslmode=disable", username, password))
 
 	if err != nil {
 		log.Fatal(err)
